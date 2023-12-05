e4409b2025c4473a3bf0d8ce156f87cbeebab95a
Ahindrila <ahindrila.dutta@pwc.com>
2023-12-05 21:34:12 +0530

    modified code

--- bin/README.md
+++ bin/README.md
@@ -1,26 +1,46 @@
 # Project Title
 
 Address Book Backend
-This project has 3 address books by default with 3 contacts each. The user can add, delete, edit contact details. The user can also create new address books or delete existing ones
+This project has 3 address books by default with 3 contacts each. 
+1) The user can add new contact.
+2) The user can add new AddressBook.
+3) Find unique contacts from each address book. 
 
+## Project Description
+This project develops an address book that allows a user to store (between
+successive runs of the program) the name and phone numbers of their friends, with the
+following functionality:
+• To be able to display the list of friends and their corresponding phone numbers sorted
+by their name.
+• Given another address book that may or may not contain the same friends, display the
+list of friends that are unique to each address book (the union of all the relative
+complements). For example given:
+
+Book1 = { “Bob”, “Mary”, “Jane” }
+Book2 = { “Mary”, “John”, “Jane” }
+The friends that are unique to each address book are:
+Book1 \ Book2 = { “Bob”, “John” }
 
 ## Run Locally
 
-Clone the project
+Clone the project -- git clone https://github.com/Ahindrila1406/FriendsAddressBook.git
 
-```bash
-  git clone https://gitlab.com/address-book-group/contact-address-book.git
-```
 
 Go to the project directory
 
-```bash
-  cd contact-address-book
-```
+  cd FriendsAddressBook
+
 
 Import project in an editor
+  Import the project in any java editor and run the project (This project is created using Java 11 so please ensure compatibility). The API's should be accessible via localhost:8080
 
-```bash
-  Import the project in any java editor (preferably intellij) and run the project (This project is created using Java 11 so please ensure compatibility). The API's should be accessible via localhost:8080
-```
+## Run from Postman
+
+1) API for creating new Address book : http://localhost:8080/contactAddressBook/createAddressBook (POST method)
+2) API for creating new contact : http://localhost:8080/contactAddressBook/createContact (POST method)
+3) API for displaying contacts with respect to id: http://localhost:8080/contactAddressBook/displayContact (POST method)
+4) API for displaying existing Address Books: http://localhost:8080/contactAddressBook/displayContactsFromAddressBook (POST method)
+5) API for displaying unique contacts in the address books : http://localhost:8080/contactAddressBook/displayUniqueContacts (GET method)
+
+
 
--- src/main/java/com/assignment/addressbook/service/AddressBookServiceImpl.java
+++ src/main/java/com/assignment/addressbook/service/AddressBookServiceImpl.java
@@ -9,6 +9,7 @@ import com.assignment.addressbook.model.AddressBook;
 import com.assignment.addressbook.model.Contact;
 import com.assignment.addressbook.model.ContactIdModel;
 import com.assignment.addressbook.model.UniqueContactsModel;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.Comparator;
 import java.util.Objects;
@@ -28,6 +30,8 @@ public class AddressBookServiceImpl implements AddressBookService{
 
     @Autowired
     private ContactDao contactDetailsDao;
+    private static final Logger logger = Logger.getLogger(AddressBookServiceImpl.class.getName());
+
 
     @Override
     public List<String> fetchAddressBookNames() {
@@ -45,34 +49,32 @@ public class AddressBookServiceImpl implements AddressBookService{
         }
         AddressBookEntity addressBookEntity = new AddressBookEntity(addressBookReq.getAddressBookName());
         this.addressBookDao.save(addressBookEntity);
+        logger.info("New Address Book : " + addressBookReq.getAddressBookName() + " created");
+        
         return addressBookEntity;
     }
 
-//    @Override
-//    public String deleteAddressBook(String addressBookName) {
-//        AddressBookEntity addressBook = this.addressBookDao.findByAddressBookName(addressBookName) .orElseThrow(() -> new CustomException("No such address book exists"));
-//        this.contactDetailsDao.deleteByAddressBookName(addressBookName);
-//        this.addressBookDao.delete(addressBook);
-//        return "Address Book " + addressBookName + " with all contacts deleted";
-//    }
 
     @Override
     public ContactEntity createContact(Contact contactDetails) {
         AddressBookEntity addressBook = this.addressBookDao.findByAddressBookName(contactDetails.getAddressBookName()).orElseThrow(() -> new CustomException("No such address book exists"));
         ContactEntity contactEntity = new ContactEntity(contactDetails.getFullName(),contactDetails.getPhoneNumber(),addressBook.getAddressBookName());
         this.contactDetailsDao.save(contactEntity);
+        logger.info("New Contact : " + contactDetails.getFullName()+ " created");
         return contactEntity;
     }
 
     @Override
     public ContactEntity fetchContact(ContactIdModel id) {
         ContactEntity contactEntity = this.contactDetailsDao.findById(id.getId()).orElseThrow(() -> new CustomException("No such contact exists"));
+        logger.info("Displaying contact of Contact Id : " + id.getId());
         return contactEntity;
     }
 
     @Override
     public List<ContactEntity> fetchContactsFromAddressBook(AddressBook addressBookModel) {
         List<ContactEntity> listContacts = this.contactDetailsDao.findByAddressBookName(addressBookModel.getAddressBookName());
+        logger.info("Displaying contacts from : " + addressBookModel.getAddressBookName());
         return listContacts.stream().sorted(Comparator.comparing(ContactEntity::getFullName))
                 .collect(Collectors.toList());
 
@@ -90,6 +92,7 @@ public class AddressBookServiceImpl implements AddressBookService{
                 unqiueMap.put(contactDetail.getFullName().toLowerCase(),null);
             }
         });
+        logger.info("Displaying unique contacts from");
         return (unqiueMap.values().stream().filter(Objects::nonNull)
                 .sorted(Comparator.comparing(UniqueContactsModel::getFullName)).collect(Collectors.toList()));
     }
